8dfffdefc66e2f17150ddcfdc5e62475d5e8ff8a
Jeff <jeffrey.manful@gmail.com>
2020-09-21 11:32:11 +0100

    Remove reel

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,20 @@
+import styled from "styled-components";
 import Video from "../components/Video";
 import { videos } from "../util/videoData";
 import Layout from "../components/Layout/index";
+import { showReelVideo } from "~/util/videoData";
+
+const StyledSection = styled.section`
+  padding: 10rem;
+  background: black;
+`;
 
 function Home(): JSX.Element {
   return (
     <Layout>
-      {videos.map((v) => (
-        <section key={v.id}>
-          <Video {...v} />
-        </section>
-      ))}
+      <StyledSection>
+        <Video {...showReelVideo} />
+      </StyledSection>
     </Layout>
   );
 }
